9247f8c5bff0a64f54900855d041e6055bf35d8a
piusmons <74440275+piusmons@users.noreply.github.com>
2023-03-24 21:07:55 +0800

    cool section update

--- src/components/Cool.js
+++ src/components/Cool.js
@@ -1,9 +1,48 @@
 import React from 'react';  
+import { useState } from 'react';
+
+let memeImg = ''
+
+
+function Meme() {
+    const [image, setImage] = useState('')
+
+    function handleClick() {
+
+        fetch("https://meme-api.com/gimme")
+            .then(response => {
+                return response.json();
+            })
+            .then(function(response){
+               let memeImg = response.url
+               setImage(memeImg)
+            })
+    }
+
+    return(
+        <>
+        <img className="memeImage" src={image} ></img>
+        <button class="buttonMeme" onClick={handleClick}>press me</button>
+        </>
+    )
+}
+
+
+
 
 
 export default function Cool() {
 
     return(
-        <div className='pageSection'>Cool</div>
+        
+        <div className='pageSection'>
+            <div className='coolContainer'>
+                <Meme/>
+            </div>
+            <div className='rightCoolContainer'>wew</div>
+        
+
+
+        </div>
     )
 }
